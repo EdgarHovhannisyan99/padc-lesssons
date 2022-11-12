bc4688efeda6c01dead76fc6e42e13e86f6e31ab
Edgar <edgar.hovhannisyan@eridatech.com>
2022-11-12 14:25:58 +0400

    Done lesson &

--- index.js
+++ index.js
@@ -1,97 +1,77 @@
 "use strict";
-var _a;
-const object = {
-    "data": 10,
-    "res": 20,
-    "data1": 30,
-    "res1": 50,
-};
-class PreviousValues {
-    constructor(obj) {
-        this.obj = {};
-        this[_a] = () => {
-            let done = false;
-            let index = 0;
-            let size = Object.keys(this.obj);
-            let key = '';
-            let previous = 0;
-            return {
-                next: () => {
-                    if (index === size.length) {
-                        done = true;
-                    }
-                    index++;
-                    if (size[index - 1]) {
-                        key = size[index - 1];
-                        previous = this.obj[size[index]];
-                    }
-                    return {
-                        value: this.obj[key] + previous,
-                        done
-                    };
-                }
-            };
-        };
-        this.obj = obj;
-    }
-}
-_a = Symbol.iterator;
-const testObj = new PreviousValues(object);
-for (let i of testObj) {
-    console.log(i);
-}
-//
-// interface ICustomArray<T> {
-//     [key: string]: T;
+// ----  PROMISE TEST -----
+// --  Test 1 --
+// function testPromise() {
+//     return new Promise(function (resolve, reject) {
+//         setTimeout(function () {
+//             resolve(8)
+//         }, 2000)
+//     })
 // }
+// const res = testPromise()
 //
-//
-// class CustomArray<T> {
-//     private _state: ICustomArray<T> = {};
-//     public size: number = 0;
-//
-//     constructor(arr: Array<T> = []) {
-//         if (arr?.length > 0) {
-//             for (let i of arr) {
-//                 this.set(i)
-//             }
-//         }
-//     }
-//
+// // @ts-ignore
+// res.then(function (result:number):number {
+//    return  result + 5
+// }).then(function (result) {
 //     // @ts-ignore
-//     set(item: T): ICustomArray<T> {
-//         if(this._state[JSON.stringify(item)]){
-//             return this._state
-//         }else {
-//             this._state[JSON.stringify(item)] = item;
-//             this.size++;
-//         }
-//     }
-//
-//     [Symbol.iterator] = () => {
-//         let done = false;
-//         let i: number = 0;
-//         const keys = Object.keys(this._state);
-//         return {
-//             next: () => {
-//                 const key = keys[i];
-//                 if (i === keys.length) {
-//                     this.size = keys.length
-//                     done = true;
-//                 }
-//                 i++;
-//                 console.log(key)
-//                 return {
-//                     value: this._state[key],
-//                     done
-//                 }
+//     console.log(result + 5)
+// })
+// -- Test 2 ---
+// function randomPromise() {
+//     return new Promise(function (resolve, reject){
+//         setTimeout(() => {
+//             const num = Math.floor(Math.random() * 10)
+//             const res = num % 2
+//             if (res) {
+//                 resolve('Success: the number is even')
+//             }else {
+//                 reject('Error: the number is odd')
 //             }
-//         }
+//             resolve(res)
+//         }, 1500)
+//     })
+// }
+// randomPromise().then(function (result) {
+//     console.log(result)
+// }).catch(function (result) {
+//     console.log(result)
+// })
+// -- Test 3 --
+// const testPromise1 = new Promise(resolve => resolve('first promise'))
+// const testPromise2 = new Promise(resolve => resolve('second promise'))
+// const testPromise3 = new Promise(resolve => resolve('third promise'))
+// // const testPromise3 = new Promise((resolve, reject) => reject('third error'))
+//
+// Promise.all([testPromise1, testPromise2, testPromise3]).then(function (result) {
+//     console.log(result)
+// }).catch(function (result) {
+//     console.log(result)
+// })
+// ----  CLOSURE TEST -----
+// let a = 5
+//
+// function func(){
+//     let b = 10
+//     return function d() {
+//         let c = 3
+//         console.log(a + b + c)
 //     }
 // }
 //
-//
-// const test = new CustomArray<number>([22, 2, 2, 1]);
-// test.set(6)
-// console.log(test)
-//
+// const res = func()
+// a = 20
+// res()
+// ----- Test THIS ------
+function logName() {
+    console.log(this.name);
+}
+let obj = {
+    name: 'Poxos',
+    age: 20,
+    logName: ''
+};
+// @ts-ignore
+obj.logName = logName;
+// @ts-ignore
+obj.logName();
