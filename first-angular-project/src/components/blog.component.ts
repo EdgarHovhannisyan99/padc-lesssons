d58ffb796da03861ad5437ba37f258475ffaa141
Edgar <edgar.hovhannisyan@eridatech.com>
2022-11-16 15:30:53 +0400

    Done lesson_9

--- first-angular-project/src/components/blog.component.ts
+++ first-angular-project/src/components/blog.component.ts
@@ -7,22 +7,50 @@ import {Component} from "@angular/core";
 })
 
 export class BlogComponent {
-   data: any[] = [
-     {
-       title: 'Blog 1',
-       description: 'Description 1'
-     },
-     {
-       title: 'Blog 2',
-       description: 'Description 2'
-     },
-     {
-       title: 'Blog 3',
-       description: 'Description 3'
-     },
-     {
-       title: 'Blog 4',
-       description: 'Description 5'
-     }
-   ]
+  private generateColor = function (): string {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+  }
+
+  data: any[] = [
+    {
+      id: 1,
+      title: 'Babken',
+      color: {'background-color': this.generateColor()},
+      description: 'Description 1',
+
+    },
+    {
+      id: 2,
+      title: 'Vazgen',
+      description: 'Description 2',
+      color: {'background-color': this.generateColor()},
+
+    },
+    {
+      id: 3,
+      title: 'Karen',
+      color: {'background-color': this.generateColor()},
+      description: 'Description 3',
+
+    },
+    {
+      id: 4,
+      title: 'Zaven',
+      color: {'background-color': this.generateColor()},
+      description: 'Description 5',
+    }
+  ]
+
+
+  public onClose = (id: number): void => {
+    const index = this.data.findIndex(i => i.id === id)
+    this.data.splice(index, 1)
+  }
+
+  public onChangeColor = (id: number): void => {
+    const index = this.data.findIndex(i => i.id === id)
+    const color = this.generateColor()
+    console.log(this.data[index].color['background-color'])
+    this.data[index].color['background-color'] = color
+  }
 }
