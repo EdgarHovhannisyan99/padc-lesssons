c8b744dda32dd471513677b156285d9e1dc71cb0
Edgar <edgar.hovhannisyan@eridatech.com>
2022-11-19 14:23:02 +0400

    less 10

--- first-angular-project/src/app/app.module.ts
+++ first-angular-project/src/app/app.module.ts
@@ -1,20 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import {BlogComponent} from "../components/blog.component";
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BlogComponent} from "../components/blog/blog.component";
+import {HeaderComponent} from "../components/header/header.component";
+import {BannerComponent} from "../components/banner/banner.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    BlogComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        BannerComponent,
+        BlogComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
